537eb00aabed826d3d2bf93ee141acc84dfe147a
practiseo <practiseo@gmail.com>
2025-12-13 16:37:13 +0100

    feat: add i18next configuration with language detection and backend support

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './i18n';
+import './i18n_new';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
